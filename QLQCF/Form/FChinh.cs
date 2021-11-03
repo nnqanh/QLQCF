e814a35423ff0eb4250c8968bdb89b649d85cdb2
Boo <191121521108@due.udn.vn>
2021-11-04 00:51:02 +0700

    Revert "Revert "Tạo form cơ bản""
    This reverts commit 6030e6eabbb186b1454af376bf176ff61120600a.

---
+++ QLQCF/Form/FChinh.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLQCF
+{
+    public partial class FChinh : Form
+    {
+        public FChinh()
+        {
+            InitializeComponent();
+        }
+
+        private void lbExit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn thoát chương trình?", "Thông báo", MessageBoxButtons.OKCancel) == System.Windows.Forms.DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void muaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FQuanly f = new FQuanly();
+            this.Hide();
+            f.ShowDialog();
+            this.Show();
+        }
+
+        private void banToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FQuanlyBan f = new FQuanlyBan();
+            this.Hide();
+            f.ShowDialog();
+            this.Show();
+        }
+
+        private void DangxuatToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FDangNhap f = new FDangNhap();
+            this.Hide();
+            f.ShowDialog();
+        }
+    }
+}
